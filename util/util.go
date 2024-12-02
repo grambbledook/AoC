c923c76a390d8d72e4cbeecb62ac247ebf5b2814
grambbledook <vodopyanovd@gmail.com>
2024-12-02 14:24:00 +0100

    Advent 2024 - Day 2

--- util/util.go
+++ util/util.go
@@ -1,8 +1,28 @@
 package util
 
+import (
+	"github.com/grambbledook/adventofcode2023/assert"
+	"strconv"
+	"strings"
+)
+
 func Must[T any](t T, err error) T {
 	if err != nil {
 		panic(err)
 	}
 	return t
+}
+
+func Ints(input string) []int {
+	in := strings.Fields(input)
+	out := make([]int, len(in))
+
+	for i, str := range in {
+		num, err := strconv.Atoi(str)
+		assert.NoError(err)
+
+		out[i] = num
+	}
+
+	return out
 }
