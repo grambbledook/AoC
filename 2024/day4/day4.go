13302198d6e0b6b13d3282d9f8f3969ef1e753e0
grambbledook <vodopyanovd@gmail.com>
2024-12-05 01:14:51 +0100

    Advent 2024 - Day 4

--- 2024/day4/day4.go
+++ 2024/day4/day4.go
@@ -2,37 +2,139 @@ package day4
 
 import (
 	"github.com/grambbledook/adventofcode2023/util"
+	"iter"
 )
 
 const SearchWord = "XMAS"
 
+type Grid struct {
+	n int
+	m int
+}
+
+func (t *Grid) oob(i, j int) bool {
+	return !t.noob(i, j)
+}
+
+func (t *Grid) noob(i, j int) bool {
+	if i < 0 || j < 0 {
+		return false
+	}
+
+	if i >= t.n || j >= t.m {
+		return false
+	}
+
+	return true
+}
+
+func (t *Grid) iter() iter.Seq2[int, int] {
+	return func(yield func(int, int) bool) {
+		for i := 0; i < t.n; i++ {
+			for j := 0; j < t.m; j++ {
+				if !yield(i, j) {
+					return
+				}
+			}
+		}
+	}
+}
+
 func Task1(file string) int {
 	lines := util.ReadLines(file)
 
-	n, m := len(lines), len(lines[0])
-
-	type Key struct {
-		x int
-		y int
-	}
-
 	var result int
+	grid := Grid{len(lines), len(lines[0])}
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < m; j++ {
-			if lines[i][j] != 'X' {
+	for i, j := range grid.iter() {
+		if lines[i][j] != 'X' {
+			continue
+		}
+
+		counts := [8]int{}
+		for k := 0; k < len(SearchWord); k++ {
+			next := SearchWord[k]
+
+			if grid.noob(i+k, j) && lines[i+k][j] == next {
+				counts[0]++
+			}
+
+			if grid.noob(i, j+k) && lines[i][j+k] == next {
+				counts[1]++
+			}
+
+			if grid.noob(i+k, j+k) && lines[i+k][j+k] == next {
+				counts[2]++
+			}
+
+			if grid.noob(i-k, j+k) && lines[i-k][j+k] == next {
+				counts[3]++
+			}
+
+			if grid.noob(i+k, j-k) && lines[i+k][j-k] == next {
+				counts[4]++
+			}
+
+			if grid.noob(i-k, j-k) && lines[i-k][j-k] == next {
+				counts[5]++
+			}
+
+			if grid.noob(i-k, j) && lines[i-k][j] == next {
+				counts[6]++
+			}
+
+			if grid.noob(i, j-k) && lines[i][j-k] == next {
+				counts[7]++
+			}
+		}
+
+		for _, count := range counts {
+			if count != len(SearchWord) {
 				continue
 			}
+			result += 1
+		}
+	}
 
-			idx := 0
-			for k := 0; k < min(n, i+4); k++ {
-				if lines[i+k][j] != SearchWord[idx] {
-					break
-				}
-				idx++
-			}
-			result += util.FromBool(idx == len(SearchWord))
+	return result
+}
 
+func Task2(file string) int {
+	lines := util.ReadLines(file)
+
+	var result int
+	grid := Grid{len(lines), len(lines[0])}
+
+	for i, j := range grid.iter() {
+		if lines[i][j] != 'A' {
+			continue
+		}
+
+		if grid.oob(i-1, j-1) || grid.oob(i+1, j+1) {
+			continue
+		}
+
+		if grid.oob(i+1, j-1) || grid.oob(i-1, j+1) {
+			continue
+		}
+
+		if lines[i-1][j-1] == lines[i+1][j+1] {
+			continue
+		}
+
+		if lines[i+1][j-1] == lines[i-1][j+1] {
+			continue
+		}
+
+		counts := [26]int{}
+
+		counts[lines[i-1][j-1]-'A'] += 1
+		counts[lines[i+1][j+1]-'A'] += 1
+		counts[lines[i-1][j+1]-'A'] += 1
+		counts[lines[i+1][j-1]-'A'] += 1
+
+		if counts['M'-'A'] == 2 && counts['S'-'A'] == 2 {
+			result += 1
 		}
 	}
 
--- 2024/test/day4_test.go
+++ 2024/test/day4_test.go
@@ -13,63 +13,20 @@ func Test_Day4_Task1_TestData(t *testing.T) {
 
 func Test_Day4_Task1_InputData(t *testing.T) {
 	result := day4.Task1("../day4/input")
-	assert.Equal(t, 173785482, result)
+	assert.Equal(t, 2297, result)
 }
 
 func Test_Day4_Task2_TestData(t *testing.T) {
-	//result := day4.Task2("../day4/test2")
-	//assert.Equal(t, 48, result)
+	result := day4.Task2("../day4/test1")
+	assert.Equal(t, 9, result)
+}
+
+func Test_Day4_Task2_TestData_Lean(t *testing.T) {
+	result := day4.Task2("../day4/test.lean")
+	assert.Equal(t, 9, result)
 }
 
 func Test_Day4_Task2_InputData(t *testing.T) {
-	//result := day4.Task2("../day4/input")
-	//assert.Equal(t, 83158140, result)
-}
-
-func Test_Day4_Util_Func_Virgin(t *testing.T) {
-	//tests := []struct {
-	//	input    string
-	//	expected int
-	//}{
-	//	{"mul(44,46)", 2024},
-	//	{"mul(123,4)", 492},
-	//	{"mul(123,4))", 492},
-	//	{"do()mul(123,4)", 492},
-	//	{"don't()mul(123,4)", 492},
-	//	{"mul(4*", 0},
-	//	{"mul(6,9!", 0},
-	//	{"?(12,34)", 0},
-	//	{"mul ( 2 , 4 )", 0},
-	//}
-	//for _, test := range tests {
-	//	result := day4.Parse(test.input, false)
-	//	if result != test.expected {
-	//		t.Errorf("Test failed. Input: %v, Expected: %v, Result: %v", test.input, test.expected, result)
-	//	}
-	//}
-}
-
-func Test_Day4_Util_Func_Chad(t *testing.T) {
-	//tests := []struct {
-	//	input    string
-	//	expected int
-	//}{
-	//	{"mul(44,46)", 2024},
-	//	{"mul(123,4)", 492},
-	//	{"do()mul(123,4)", 492},
-	//	{"don't()mul(123,4)", 0},
-	//	{"don't()mul(123,4)mul(1,5)", 0},
-	//	{"don't()_do()_mul(123,4)", 492},
-	//	{"don't()\nmul(123,4)mul(1,5)", 0},
-	//	{"mul(4*", 0},
-	//	{"mul(6,9!", 0},
-	//	{"?(12,34)", 0},
-	//	{"mul ( 2 , 4 )", 0},
-	//}
-	//for _, test := range tests {
-	//	result := day4.Parse(test.input, true)
-	//	if result != test.expected {
-	//		t.Errorf("Test failed. Input: %v, Expected: %v, Result: %v", test.input, test.expected, result)
-	//	}
-	//}
+	result := day4.Task2("../day4/input")
+	assert.Equal(t, 1745, result)
 }
